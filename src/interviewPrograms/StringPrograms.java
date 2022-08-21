d1eb5df586e60caa90a7360bda65f32666e63c21
Rajesh Swarnkar <rajesh.swarnkar@gmail.com>
2022-08-21 15:23:29 +0530

    learn Character class

--- src/interviewPrograms/StringPrograms.java
+++ src/interviewPrograms/StringPrograms.java
@@ -6,10 +6,13 @@ public class StringPrograms {
 		// TODO Auto-generated method stub
 
 		//find sum of integers from a given string
-		String str1 = "Code7'as'+87 2Poin";
+		String str1 = "Code7'as'+87 2Point";
+		int len = str1.length();
 		sumOfIntegersInString(str1);
+		sumOfIntegersInString(str1,len);
 	}
 	
+	//learn concept of Char to int using Character.getNumericValue()
 	public static void sumOfIntegersInString(String str1) {
 		int sum = 0;
 		
@@ -28,4 +31,22 @@ public class StringPrograms {
 		
 	}
 
+	//learn concept of Char to int using String.valueOf()
+	public static void sumOfIntegersInString(String str1, int len) {
+		int sum = 0;
+		
+		if (len > 0) {
+			
+			for(int i=0;i<str1.length();i++) {
+				char ch = str1.charAt(i);
+
+				if(Character.isDigit(ch)) {
+					sum = sum + Integer.parseInt(String.valueOf(ch));
+				}
+			}
+			
+		}
+		System.out.println("sum of integers is: " + sum);
+		
+	}
 }
